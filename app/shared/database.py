b90bb51bb96ec52098522ba81b63a34c5fcd5ab4
Eduardo Schulz <eduardo.schulz@protonmail.com>
2026-05-28 21:12:05 -0300

    setting up sqlalchemy engine

---
+++ app/shared/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from shared.config import settings
+
+engine = create_engine(settings.DATABASE_URL)
+
+Session = sessionmaker(engine)
+
+
+# with Session() as s:
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
