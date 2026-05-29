1fadd8173a14b56c9bb6234058c065bf58b90d44
Eduardo Schulz <eduardo.schulz@protonmail.com>
2026-05-28 21:37:24 -0300

    fixing some stuff to allow tests to pass

--- app/shared/config.py
+++ app/shared/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     # sets path to where pydantics will look for the .env file; in this case outside of app/
-    model_config = SettingsConfigDict(env_file="../.env")
+    model_config = SettingsConfigDict(env_file=".env")
 
     DATABASE_URL: str
     JWT_SECRET: str
