e3219d1cf122f0f8a3c2dd83495f5abdc85bbf2c
Eduardo Schulz <eduardo.schulz@protonmail.com>
2026-05-28 23:30:15 -0300

    fixing setup for alembic

--- app/shared/config.py
+++ app/shared/config.py
@@ -1,9 +1,13 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+ENV_FILE = Path(__file__).resolve().parent.parent.parent / ".env"
 
 
 class Settings(BaseSettings):
     # sets path to where pydantics will look for the .env file; in this case outside of app/
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=ENV_FILE)
 
     DATABASE_URL: str
     JWT_SECRET: str
