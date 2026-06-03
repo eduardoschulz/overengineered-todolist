9d269043694e9b014cf9c03d289cb76a3272c822
eduardoschulz <eduardo.schulz@protonmail.com>
2026-06-03 20:09:26 -0300

    adding exceptions class for fastapi

--- app/main.py
+++ app/main.py
@@ -1,16 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from shared.exceptions import AppError
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+def root():
     return {"message": "Hello World!"}
 
 
 @app.get("/health", status_code=200)
-async def health():
+def health():
     return {"status": "ok"}
+
+
+@app.exception_handler(AppError)
+def app_error_handler(request: Request, exc: AppError) -> JSONResponse:
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": str(exc)},
+    )
+
 
 # @app.get("/users/")
 # def read_users(db: Session = Depends(get_db)):
